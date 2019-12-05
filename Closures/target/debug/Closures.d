c8e4922eec93dd370abe90c900947f5461830803
Your Name <Your Email>
2019-12-05 05:12:30 +0000

    added commit

---
+++ Closures/target/debug/Closures.d
@@ -0,0 +1,1 @@
+/home/nida/Closures_properties.rs/Closures/target/debug/Closures: /home/nida/Closures_properties.rs/Closures/src/main.rs
---
+++ Closures/target/debug/deps/Closures-99c64f5c81c63320.d
@@ -0,0 +1,5 @@
+/home/nida/Closures_properties.rs/Closures/target/debug/deps/Closures-99c64f5c81c63320: src/main.rs
+
+/home/nida/Closures_properties.rs/Closures/target/debug/deps/Closures-99c64f5c81c63320.d: src/main.rs
+
+src/main.rs:
